b1b28c666504fdd6366bdbe17b0c7fa73e6f8f12
Pirlopower420 <jhonhamiltongomezmolina22@gmail.com>
2025-04-30 20:27:31 -0500

    lo nuevo del 30/04/2025

--- js/longinservice.js
+++ js/longinservice.js
@@ -8,25 +8,42 @@ document.getElementById("formLogin").addEventListener('submit' , function(e){
 function login(email, password){
      let message = ""
      let alerType = ""
+     localStorage.removeItem("token")
     fetch("https://reqres.in/api/login",{
         method: "POST",
         headers: {
-            "Content-type": "application/json"
+            "Content-type": "application/json",
+            "x-api-key": "reqres-free-v1"
         },
         body: JSON.stringify({email, password})
     })
-    .then((data) => {
-        alerType = "success"
-        message = 'Incio de secion exitosa'
-        console.log("Responde bien")
+    .then((response) => {
+        if(response.status === 200){
+            alerType = "success"
+            message = 'Incio de secion exitosa'
+            console.log("Responde bien"+ response)
+            alertbuilder(alerType,message)
+            localStorage.setItem("token", "1111111111")
+            setTimeout(() => {
+                  location.href= "admin/dashboard.html"
+            }, 2000) // 2000 ms = 2 segundos 
+           
+        }else{
+             alerType = "danger"
+             message = "correo o contraseña incorrecta"
+             alertbuilder(alerType,message)
+
+        }
+       
     })
     .catch((error) => {
-          alerType = "danger"
-        message = "correo o contraseña incorrecta"
         console.error(error)
     })
+}
+   
 
-    let alert = `
+    function alertbuilder(alerType, message){
+        let alert = `
     <div class="alert alert-${alerType} alert-dismissible fade show" role="alert">
         ${message}
         <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
